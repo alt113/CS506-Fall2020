be7a51cfc79ee7db0ac9b35e48e01662d8c637e0
Rayyan Nasr <developer.rayyan.nasr@gmail.com>
2020-09-10 20:07:05 +0300

    Added gym drawing implementation under '01-python/leisure/gym.py'

--- 01-python/leisure/gym.py
+++ 01-python/leisure/gym.py
@@ -1,12 +1,3 @@
 def draw_gym():
-    data = [['+---------------------+'], ['| Welcome to Gym 506! |'],
-            ['+---------------------+'], ['|        _ _  hey     |'],
-            ['|        | |  O/      |'], ['|        ||| /|       |'],
-            ['|        | |  /\      |'], ['+---------------------+']]
-
-    col_width = max(len(word) for row in data for word in row) + 2  # padding
-    for row in data:
-        print "".join(word.ljust(col_width) for word in row)
-
-
-draw_gym()
+    print("gym not found")
+    return
