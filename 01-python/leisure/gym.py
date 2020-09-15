e2455566b01a8d8fed020a3dec4f38f3108e35a0
Lance <lgallett@redhat.com>
2020-09-15 10:23:22 -0400

    Updating: adding dataset 2
    Delete alt113.txt
    
    Added gym drawing implementation under '01-python/leisure/gym.py'
    
    Added gym drawing implementation under '01-python/leisure/gym.py'
    
    Added gym drawing implementation under '01-python/leisure/gym.py'
    
    separated git from oython exercises
    
    Added gym drawing implementation under '01-python/leisure/gym.py'
    
    added gym drawing feature

--- 01-python/leisure/gym.py
+++ 01-python/leisure/gym.py
@@ -1,3 +1,10 @@
 def draw_gym():
-    print("gym not found")
-    return
+    data = [['+---------------------+'], ['| Welcome to Gym 506! |'],
+            ['+---------------------+'], ['|        _ _  hey     |'],
+            ['|        | |  O/      |'], ['|        ||| /|       |'],
+            ['|        | |  /\      |'], ['+---------------------+']]
+
+    col_width = max(len(word) for row in data for word in row) + 2  # padding
+    for row in data:
+        print "".join(word.ljust(col_width) for word in row)
+
